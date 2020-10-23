9177a2f9f5e5ce57e50826410982d8e098041674
DieterDoman <dieter.doman@takealot.com>
2020-10-23 17:23:56 +0200

    Add token endpoint.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(bodyParser.json());
+
+app.post('/facebook_login', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
 })
 
 app.listen(port, () => {
