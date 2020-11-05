1c6b7106834354818717c188386906850d2c1276
DieterDoman <dieter.doman@takealot.com>
2020-11-05 16:13:50 +0200

    Use access token to call facebook api.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import axios from 'axios';
+import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json());
 
 app.post('/facebook_login', (req, res) => {
-    console.log(req.body);
+    const accessToken = req.body.accessToken;
+    const userId = req.body.userId;
+    axios.get(`https://graph.facebook.com/${userId}?fields=email&access_token=${accessToken}`)
+    .then((response) => {
+        console.log(response);
+    });
     res.sendStatus(200);
 })
 
